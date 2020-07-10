bf899810dc4597e516cdb42e000d77e44accb80e
Sanatkumar Sadakale <sadakale@gmail.com>
2020-07-10 10:49:19 +1000

    landing page cleanup and scss setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-optus-progress-bar'`, () => {
+  it(`should have as title 'Optus Progress bar demo application using angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-optus-progress-bar');
+    expect(app.title).toEqual('Optus Progress bar demo application using angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-optus-progress-bar app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('progress bars with buttons here');
   });
 });
