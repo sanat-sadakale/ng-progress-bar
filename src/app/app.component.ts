cb76e39a4c4822805c6b918023bfde837551066e
Sanatkumar Sadakale <sadakale@gmail.com>
2020-07-10 17:08:35 +1000

    main page with click actions and bar selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { Component, ViewChildren, QueryList } from '@angular/core';
 import { BarsService, BarsConfiguration } from './services/bars.service';
 
 @Component({
@@ -7,25 +8,32 @@ import { BarsService, BarsConfiguration } from './services/bars.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'Optus Progress bar demo application using angular';
 
-  private _barsData : BarsConfiguration;
+  private _barsData: BarsConfiguration;
   public get barsData(): BarsConfiguration {
     return this._barsData || {};
   }
 
-  constructor(private barServiceClient: BarsService){
+  @ViewChildren(ProgressBarComponent) bars!: QueryList<ProgressBarComponent>;
+  public selectBarId: number;
+
+  constructor(private barServiceClient: BarsService) {
     barServiceClient.getBarsConfigurations().subscribe((response: BarsConfiguration) => {
-      this._barsData =  response;
+      this._barsData = response;
     }, (error: any) => {
       console.log(`Error in retrieving bars details: ${error.message}`);
 
     });
   }
+
   /**
    * onButtonClick
    */
   public onButtonClick(barIndex: number, stepValue: number, limit: number) {
-    console.log(`params: index: ${barIndex}, step: ${stepValue}, limit: ${limit}`);
+    const selectedBar = this.bars.find(p => p.id === barIndex);
+    if (!this.selectBarId) { return; }
+    selectedBar?.updateProgress(stepValue, limit);
   }
 }
